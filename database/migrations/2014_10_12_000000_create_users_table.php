3e9b60889d31b90d063da7dc9f1802aeda0e057b
ahsanalamgir14 <85336262+ahsanalamgir14@users.noreply.github.com>
2022-03-03 06:45:19 +0500

    Auth pages and bootstrap added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,8 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,11 +16,23 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name')->nullable()->default(null);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->longText('password');
+            $table->string('role')->nullable()->default('customer');
+            $table->string('mobile')->nullable()->default(null);
+            $table->string('address')->nullable()->default(null);
+            $table->string('city')->nullable()->default(null);
+            $table->string('state')->nullable()->default(null);
+            $table->string('country')->nullable()->default(null);
+            $table->string('zip_code')->nullable()->default(null);
+            $table->string('rand_id')->nullable()->default(null);
+            $table->integer('is_verified')->nullable()->default('0');
+            $table->integer('is_forgot_password')->nullable()->default('0');
+            $table->integer('status');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -33,4 +46,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
---
+++ database/migrations/2021_08_02_000000_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); //one to one
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
